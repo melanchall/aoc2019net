2eeca8699ca5db4a6e1c0ad1cb44948350ef2566
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-12-05 13:00:57 +0300

    Prepared day 6

--- Aoc2019Net/Day.cs
+++ Aoc2019Net/Day.cs
@@ -17,21 +17,29 @@ namespace Aoc2019Net
 
         public (object Part1, object Part2) Solve()
         {
-            return (SolvePart1(), SolvePart2());
+            object part1 = null, part2 = null;
+
+            try { part1 = SolvePart1(); } catch { }
+            try { part2 = SolvePart2(); } catch { }
+
+            return (part1, part2);
         }
 
         protected abstract object SolvePart1();
 
         protected abstract object SolvePart2();
 
-        protected string[] GetInputTokens(string delimiter) => (Input ?? File.ReadAllText(GetInputFilePath())).Split(delimiter);
+        protected string[] GetInputLines() => GetInputTokens(Environment.NewLine);
 
-        protected string[] GetInputLines() => (Input != null ? Input.Split(Environment.NewLine) : File.ReadAllLines(GetInputFilePath()))
+        protected string[] GetInputTokens(string delimiter) => GetInput()
+            .Split(delimiter)
             .Where(line => !string.IsNullOrWhiteSpace(line))
             .Select(line => line.Trim())
             .ToArray();
 
-        protected int[] GetInputNumbers(string delimiter) => GetInputTokens(delimiter).Select(t => int.Parse(t.Trim())).ToArray();
+        protected int[] GetInputNumbers(string delimiter) => GetInputTokens(delimiter).Select(t => int.Parse(t)).ToArray();
+
+        private string GetInput() => Input ?? File.ReadAllText(GetInputFilePath());
 
         private string GetInputFilePath() => Path.Combine("Inputs", $"Day{DayNumber}.txt");
     }
---
+++ Aoc2019Net/Days/Day6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aoc2019Net.Days
+{
+    public sealed class Day6 : Day
+    {
+        protected override object SolvePart1()
+        {
+            return null;
+        }
+
+        protected override object SolvePart2()
+        {
+            return null;
+        }
+    }
+}
