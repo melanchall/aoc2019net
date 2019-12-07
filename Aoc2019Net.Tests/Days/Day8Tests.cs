a77b118a5e03679fbe11f690670eb1bf370cfff6
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-12-07 21:00:31 +0300

    Prepared day 8

---
+++ Aoc2019Net.Tests/Days/Day8Tests.cs
@@ -0,0 +1,14 @@
+﻿using Aoc2019Net.Tests.Attributes;
+using NUnit.Framework;
+
+namespace Aoc2019Net.Tests.Days
+{
+    [TestFixture]
+    [DayDataPart1(null)]
+    [DayDataPart1(@"", null)]
+    [DayDataPart2(null)]
+    [DayDataPart2(@"", null)]
+    public sealed class Day8Tests : DayTests<Day8Tests>
+    {
+    }
+}
