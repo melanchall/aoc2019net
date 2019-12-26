5f8b5c378f84a05c74d7583c4d5e61bc18dd379c
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2019-12-26 23:00:41 +0300

    Day 16 solved (part 1)

--- Aoc2019Net.Tests/Days/Day16Tests.cs
+++ Aoc2019Net.Tests/Days/Day16Tests.cs
@@ -4,8 +4,15 @@ using NUnit.Framework;
 namespace Aoc2019Net.Tests.Days
 {
     [TestFixture]
-    [DayDataPart1(null)]
+    [DayDataPart1("44098263")]
+    [DayDataPart1("12345678", "01029498", Parameters = new object[] { 4 })]
+    [DayDataPart1("80871224585914546619083218645595", "24176176")]
+    [DayDataPart1("19617804207202209144916044189917", "73745418")]
+    [DayDataPart1("69317163492948606335995924319873", "52432133")]
     [DayDataPart2(null)]
+    [DayDataPart2("03036732577212944063491565474664", "84462026")]
+    [DayDataPart2("02935109699940807407585447034323", "78725270")]
+    [DayDataPart2("03081770884921959731165446850517", "53553731")]
     public sealed class Day16Tests : DayTests<Day16Tests>
     {
     }
--- Aoc2019Net/Days/Day16.cs
+++ Aoc2019Net/Days/Day16.cs
@@ -1,10 +1,56 @@
-﻿namespace Aoc2019Net.Days
+﻿using System;
+using System.Linq;
+
+namespace Aoc2019Net.Days
 {
     public sealed class Day16 : Day
     {
         public override object SolvePart1()
         {
-            return null;
+            var steps = Convert.ToInt32((Parameters ?? new object[] { 100 })[0]);
+            var signal = GetInput().Select(c => int.Parse(c.ToString())).ToArray();
+
+            var basePattern = new[] { 0, 1, 0, -1 };
+            var patterns = signal.Select((_, i) => Enumerable.Range(0, int.MaxValue)
+                                                             .SelectMany(_ => basePattern.SelectMany(nn => Enumerable.Range(0, i + 1).Select(_ => nn)))
+                                                             .Skip(1)
+                                                             .Take(signal.Length)
+                                                             .ToArray())
+                                 .ToArray();
+
+            var tmp = new int[signal.Length];
+
+            for (var s = 0; s < steps; s++)
+            {
+                for (var i = 0; i < signal.Length; i++)
+                {
+                    var sum = 0;
+
+                    for (var k = i; k < signal.Length; k += 4 * (i + 1))
+                    {
+                        for (var j = 0; j < i + 1 && k + j < signal.Length; j++)
+                        {
+                            sum += signal[k + j];
+                        }
+                    }
+
+                    for (var k = (i + 1) * 3 - 1; k < signal.Length; k += 4 * (i + 1))
+                    {
+                        for (var j = 0; j < i + 1 && k + j < signal.Length; j++)
+                        {
+                            sum -= signal[k + j];
+                        }
+                    }
+
+                    tmp[i] = Math.Abs(sum % 10);
+                }
+
+                var t = signal;
+                signal = tmp;
+                tmp = t;
+            }
+
+            return string.Join(string.Empty, signal.Take(8));
         }
 
         public override object SolvePart2()
